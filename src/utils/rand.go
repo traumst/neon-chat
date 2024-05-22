cf5706496af2990a8b4a817b1c041ba951da6c9b
Alex L <alexlitvak90@gmail.com>
2024-05-22 23:56:11 +0300

    better random string bytes

--- src/utils/rand.go
+++ src/utils/rand.go
@@ -4,8 +4,10 @@ import "math/rand"
 
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
+	len := len(LetterBytes)
 	for i := range b {
-		b[i] = LetterBytes[rand.Intn(len(LetterBytes))]
+		idx := rand.Intn(len)
+		b[i] = LetterBytes[idx]
 	}
 	return string(b)
 }
