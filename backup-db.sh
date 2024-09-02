198a331afc1971741e51fb9f4b24f48d92634649
Alex L <alexlitvak90@gmail.com>
2024-09-02 21:22:56 +0300

    add db backup and metric scripts to run.sh

--- backup-db.sh
+++ backup-db.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+# call it like `./backup-db.sh -in chat.db -out ./backup`
 # creates ./backup folder if needed
 # dumps chat.db to ./backup/chat.db-backup-2024-13-35T76:90:65Z.sql
 
+if [ "$1" != "-in" ] || [ "$3" != "-out" ]; then
+  echo "required format: $0 -in <sqlite_db_file> -out <backup_folder>"
+  echo "you provided:    $0 $1 $2 $3 $4"
+  exit 1
+fi
+DATABASE_FILE=$2
+BACKUP_FOLDER_RELATIVE_PATH=$4
+#DATABASE_FILE="chat.db"
+#BACKUP_FOLDER_RELATIVE_PATH="./backup"
+
 ISO_TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-BACKUP_FOLDER_RELATIVE_PATH="backup"
-DATABASE_FILE="chat.db"
-BACKUP_FILE="./$BACKUP_FOLDER_RELATIVE_PATH/$DATABASE_FILE-backup-$ISO_TIMESTAMP.sql"
+BACKUP_FILE="$BACKUP_FOLDER_RELATIVE_PATH/$DATABASE_FILE-backup-$ISO_TIMESTAMP.sql"
 
 if [ ! -d "$BACKUP_FOLDER_RELATIVE_PATH" ]; then
   mkdir "$BACKUP_FOLDER_RELATIVE_PATH"
