b2b93ed4b3a969b82a50e615a16eaa6cdaf1d9c5
Alex L <alexlitvak90@gmail.com>
2024-11-26 20:11:44 +0200

    remove 1 db conn limitation

--- src/db/db_conn.go
+++ src/db/db_conn.go
@@ -35,9 +35,6 @@ func ConnectDB(dbPath string) (*DBConn, error) {
 	}
 	log.Printf("db connects to [%s]", dbPath)
 	conn, err := sqlx.Connect("sqlite3", dbPath)
-	conn.DB.SetMaxOpenConns(1)
-	conn.DB.SetMaxIdleConns(1)
-	conn.DB.SetConnMaxLifetime(0)
 	if err != nil {
 		return nil, fmt.Errorf("error opening db: %s", err)
 	}
