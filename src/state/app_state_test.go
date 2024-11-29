f82a570c70fea4666074aafe3f6ba47cba48f8e2
Alex L <alexlitvak90@gmail.com>
2024-11-29 15:37:36 +0200

    add user open chat persistance

--- src/state/app_state_test.go
+++ src/state/app_state_test.go
@@ -1,12 +1,16 @@
 package state
 
 import (
+	"fmt"
 	"math/rand"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"neon-chat/src/app"
 	"neon-chat/src/app/enum"
+	"neon-chat/src/utils"
 	"neon-chat/src/utils/config"
 	h "neon-chat/src/utils/http"
 )
@@ -15,15 +19,14 @@ func TestStateDefaults(t *testing.T) {
 	app1 := &GlobalAppState
 	app1.Init(config.Config{})
 	if app1.isInit != true {
-		t.Errorf("TestStateDefaults expected isInit true, got [%v]", app1.isInit)
+		t.Errorf("expected isInit true, got [%v]", app1.isInit)
 	}
 	if app1.config.CacheSize != 1024 {
-		t.Errorf("TestStateDefaults expected cache size 1024, got [%d]", app1.config.CacheSize)
+		t.Errorf("expected cache size 1024, got [%d]", app1.config.CacheSize)
 	}
 }
 
 func TestAddConn(t *testing.T) {
-	t.Logf("TestAddConn started")
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/some-route", nil)
 	user := app.User{Id: 1, Name: "John", Type: enum.UserType(enum.UserTypeBasic)}
@@ -31,19 +34,18 @@ func TestAddConn(t *testing.T) {
 	app1.Init(config.Config{})
 	conn1 := app1.AddConn(w, *r, &user, nil)
 	if conn1 == nil {
-		t.Fatalf("TestAddConn expected a conn1, got nil")
+		t.Fatalf("expected a conn1, got nil")
 	}
 	conn2 := app1.AddConn(w, *r, &user, nil)
 	if conn2 == nil {
-		t.Fatalf("TestAddConn expected a conn2, got nil")
+		t.Fatalf("expected a conn2, got nil")
 	}
 	if conn1 == conn2 {
-		t.Fatalf("TestAddConn expected conn1 and conn2 to be different")
+		t.Fatalf("expected conn1 and conn2 to be different")
 	}
 }
 
 func TestGetConn(t *testing.T) {
-	t.Logf("TestGetConn started")
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/some-route", nil)
 	reqId := "test-req-id"
@@ -53,11 +55,11 @@ func TestGetConn(t *testing.T) {
 	app1.Init(config.Config{})
 	expect := app1.AddConn(w, *r, &user, nil)
 	if expect == nil {
-		t.Fatalf("TestGetConn expected a conn, got nil")
+		t.Fatalf("expected a conn, got nil")
 	}
 	conns := app1.GetConn(user.Id)
 	if len(conns) == 0 {
-		t.Fatalf("TestGetConn expected connections were empty, count [%d]", len(conns))
+		t.Fatalf("expected connections were empty, count [%d]", len(conns))
 	}
 	var conn *Conn
 	for _, c := range conns {
@@ -67,24 +69,23 @@ func TestGetConn(t *testing.T) {
 		}
 	}
 	if expect == nil || conn == nil {
-		t.Fatalf("TestGetConn expected connections, got\n expected[%v]\n conn[%v]", expect, conn)
+		t.Fatalf("expected connections, got\n expected[%v]\n conn[%v]", expect, conn)
 	}
 	if expect.User != conn.User {
-		t.Fatalf("TestGetConn expected equality,\nexpected user[%v],\nconn origin[%v]", expect.User, conn.User)
+		t.Fatalf("expected equality,\nexpected user[%v],\nconn origin[%v]", expect.User, conn.User)
 	}
 	if expect.Origin != conn.Origin {
-		t.Fatalf("TestGetConn expected equality,\nexpected origin[%v],\nconn origin[%v]", expect.Origin, conn.Origin)
+		t.Fatalf("expected equality,\nexpected origin[%v],\nconn origin[%v]", expect.Origin, conn.Origin)
 	}
 	if expect.In != conn.In {
-		t.Fatalf("TestGetConn expected equality,\nexpected in[%v],\nconn in[%v]", expect.In, conn.In)
+		t.Fatalf("expected equality,\nexpected in[%v],\nconn in[%v]", expect.In, conn.In)
 	}
 	if expect.Writer != conn.Writer {
-		t.Fatalf("TestGetConn expected equality,\nexpected writer[%v],\nconn writer[%v]", expect.Writer, conn.Writer)
+		t.Fatalf("expected equality,\nexpected writer[%v],\nconn writer[%v]", expect.Writer, conn.Writer)
 	}
 }
 
 func TestDropConn(t *testing.T) {
-	t.Logf("TestDropConn started")
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/some-route", nil)
 	reqId := "test-req-id"
@@ -94,24 +95,23 @@ func TestDropConn(t *testing.T) {
 	app1.Init(config.Config{})
 	expect := app1.AddConn(w, *r, &user, nil)
 	if expect == nil {
-		t.Fatalf("TestDropConn expected a conn, got nil")
+		t.Fatalf("expected a conn, got nil")
 	}
 	conns := app1.GetConn(user.Id)
 	if len(conns) == 0 {
-		t.Fatalf("TestDropConn expected connections were empty, count [%d]", len(conns))
+		t.Fatalf("expected connections were empty, count [%d]", len(conns))
 	}
 	err := app1.DropConn(expect)
 	if err != nil {
-		t.Fatalf("TestDropConn expected no error, got [%s]", err.Error())
+		t.Fatalf("expected no error, got [%s]", err.Error())
 	}
 	conns = app1.GetConn(user.Id)
 	if len(conns) != 0 {
-		t.Fatalf("TestGetConn user still has [%d] connections[%v]", len(conns), conns)
+		t.Fatalf("user still has [%d] connections[%v]", len(conns), conns)
 	}
 }
 
 func TestOpenChat(t *testing.T) {
-	t.Logf("TestOpenChat started")
 	user := app.User{
 		Id: uint(rand.Uint32()),
 		// Name: "John",
@@ -127,16 +127,15 @@ func TestOpenChat(t *testing.T) {
 	})
 	err := app1.OpenChat(user.Id, 22)
 	if err != nil {
-		t.Fatalf("TestOpenChat expected no error, %s", err.Error())
+		t.Fatalf("expected no error, %s", err.Error())
 	}
 	openChatId := app1.GetOpenChat(user.Id)
 	if openChatId != 22 {
-		t.Fatalf("TestOpenChat expected chat[22], got [%d]", openChatId)
+		t.Fatalf("expected chat[22], got [%d]", openChatId)
 	}
 }
 
 func TestGetOpenChatEmpty(t *testing.T) {
-	t.Logf("TestGetOpenChatEmpty started")
 	user := app.User{
 		Id: uint(rand.Uint32()),
 		// Name: "John",
@@ -146,12 +145,11 @@ func TestGetOpenChatEmpty(t *testing.T) {
 	app1.Init(config.Config{})
 	open := app1.GetOpenChat(user.Id)
 	if open != 0 {
-		t.Fatalf("TestGetOpenChatEmpty expected 0, got [%v]", open)
+		t.Fatalf("expected 0, got [%v]", open)
 	}
 }
 
 func TestGetOpenChat(t *testing.T) {
-	t.Logf("TestGetOpenChat started")
 	user := app.User{
 		Id: uint(rand.Uint32()),
 		// Name: "John",
@@ -161,24 +159,24 @@ func TestGetOpenChat(t *testing.T) {
 	app1.Init(config.Config{})
 	err := app1.OpenChat(user.Id, 33)
 	if err != nil {
-		t.Fatalf("TestGetOpenChat failed to open chat, %s", err.Error())
+		t.Fatalf("failed to open chat, %s", err.Error())
 	}
 	open3 := app1.GetOpenChat(user.Id)
 	if open3 != 33 {
-		t.Fatalf("TestGetOpenChat expected open chat[0], got [%d]", open3)
+		t.Fatalf("expected open chat[0], got [%d]", open3)
 	}
 	err = app1.OpenChat(user.Id, 22)
 	if err != nil {
-		t.Fatalf("TestGetOpenChat expected no error, %s", err.Error())
+		t.Fatalf("expected no error, %s", err.Error())
 	}
 	open := app1.GetOpenChat(user.Id)
 	if open != 22 {
-		t.Fatalf("TestGetOpenChat expected chat[%d], got [%d]", 22, open)
+		t.Fatalf("expected chat[%d], got [%d]", 22, open)
 	}
 }
 
 func TestCloseChat(t *testing.T) {
-	t.Logf("TestCloseChat started")
+	t.Logf("started")
 	user := app.User{
 		Id: uint(rand.Uint32()),
 		// Name: "John",
@@ -189,14 +187,93 @@ func TestCloseChat(t *testing.T) {
 	app1.OpenChat(user.Id, 11)
 	open := app1.GetOpenChat(user.Id)
 	if open != 11 {
-		t.Fatalf("TestCloseChat expected chat[%d], got [%d]", 11, open)
+		t.Fatalf("expected chat[%d], got [%d]", 11, open)
 	}
 	err := app1.CloseChat(user.Id, 11)
 	if err != nil {
-		t.Fatalf("TestCloseChat failed to close chat, %s", err.Error())
+		t.Fatalf("failed to close chat, %s", err.Error())
 	}
 	open = app1.GetOpenChat(user.Id)
 	if open != 0 {
-		t.Fatalf("TestCloseChat expected chat[%d] to be closed, but got[%v]", 11, open)
+		t.Fatalf("expected chat[%d] to be closed, but got[%v]", 11, open)
+	}
+}
+
+func TestSaveToFile(t *testing.T) {
+	t.Logf("started")
+	testFile := fmt.Sprintf("test-save-state-%d.json", rand.Uint32())
+	defer func() {
+		_ = os.Remove(testFile)
+	}()
+
+	app1 := &GlobalAppState
+	app1.Init(config.Config{CacheSize: 8})
+	_ = app1.OpenChat(1, 11)
+	_ = app1.OpenChat(2, 22)
+	_ = app1.OpenChat(3, 33)
+	err := app1.SaveToFile(testFile)
+	if err != nil {
+		t.Fatalf("expected no error, got [%s]", err.Error())
+	}
+	if app1.chats.Count() != 0 {
+		t.Fatalf("expected to empty chats, got [%d]", app1.chats.Count())
+	}
+	if openChat := app1.GetOpenChat(1); openChat != 0 {
+		t.Fatalf("expected no open chat, got [%d]", openChat)
+	}
+
+	content, err := utils.ReadFileContent(testFile)
+	if err != nil {
+		t.Fatalf("failed to read file, %s", err.Error())
+	}
+
+	if !strings.Contains(content, "\"1\":11") {
+		t.Fatalf("content missing '1':11, got [%s]", content)
+	}
+	if !strings.Contains(content, "\"2\":22") {
+		t.Fatalf("content missing '2':22, got [%s]", content)
+	}
+	if !strings.Contains(content, "\"3\":33") {
+		t.Fatalf("content missing '3':33, got [%s]", content)
+	}
+}
+
+func TestLoadFromFile(t *testing.T) {
+	t.Logf("TestLoadFromFile started")
+	testFile := fmt.Sprintf("test-load-state-%d.json", rand.Uint32())
+	defer func() {
+		_ = os.Remove(testFile)
+	}()
+
+	app1 := &GlobalAppState
+	app1.Init(config.Config{CacheSize: 8})
+	_ = app1.OpenChat(1, 11)
+	_ = app1.OpenChat(2, 22)
+	_ = app1.OpenChat(3, 33)
+	_ = app1.SaveToFile(testFile)
+	count := app1.chats.Count()
+	if count != 0 {
+		t.Fatalf("expected to empty chats, got [%d]", count)
+	}
+	if openChat := app1.GetOpenChat(1); openChat != 0 {
+		t.Fatalf("expected no open chat, got [%d]", openChat)
+	}
+
+	err := app1.LoadFromFile(testFile)
+	if err != nil {
+		t.Fatalf("failed to load file, %s", err.Error())
+	}
+	count = app1.chats.Count()
+	if count != 3 {
+		t.Fatalf("expected chat length to be 3 got [%d]", count)
+	}
+	if openChat := app1.GetOpenChat(1); openChat != 11 {
+		t.Fatalf("expected open chat 11, got [%d]", openChat)
+	}
+	if openChat := app1.GetOpenChat(2); openChat != 22 {
+		t.Fatalf("expected open chat 22, got [%d]", openChat)
+	}
+	if openChat := app1.GetOpenChat(3); openChat != 33 {
+		t.Fatalf("expected open chat 33, got [%d]", openChat)
 	}
 }
