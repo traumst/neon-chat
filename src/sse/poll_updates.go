ae897ffa7c6e05cb4e75015e8587e0e43be2e7da
Alex L <alexlitvak90@gmail.com>
2024-12-14 00:37:54 +0200

    redis stub

--- src/sse/poll_updates.go
+++ src/sse/poll_updates.go
@@ -2,7 +2,6 @@ package sse
 
 import (
 	"log"
-	"time"
 
 	"neon-chat/src/state"
 	m "neon-chat/src/utils/maintenance"
@@ -12,20 +11,20 @@ import (
 func PollUpdates(state *state.State, conn *state.Conn, pollingUserId uint) bool {
 	log.Printf("TRACE [%s] Live updates triggered by user[%d]\n", conn.Origin, conn.User.Id)
 	done := false
-	ticker := time.NewTicker(1 * time.Second)
+	//ticker := time.NewTicker(1 * time.Second)
 
 	for !done && !m.MaintenanceManager.IsInMaintenance() {
 		select {
 		case <-conn.Reader.Context().Done():
-			log.Printf("TRACE [%s] user[%d] disconnects as done:[%t]\n", conn.Origin, conn.User.Id, done)
-			return true
+			//log.Printf("TRACE [%s] user[%d] disconnects as done:[%t]\n", conn.Origin, conn.User.Id, done)
+			done = true
 		case up := <-conn.In:
-			log.Printf("TRACE [%s] user[%d] is receiving update[%s]\n", conn.Origin, conn.User.Id, up.Event)
+			//log.Printf("TRACE [%s] user[%d] is receiving update[%s]\n", conn.Origin, conn.User.Id, up.Event)
 			conn.SendUpdates(up, conn.User.Id)
-		case <-ticker.C:
+			//case <-ticker.C:
 			//log.Printf("TRACE [%s] user[%d] still polling updates\n", conn.Origin, conn.User.Id)
 		}
 	}
 	log.Printf("TRACE [%s] user[%d] polling updates stopped\n", conn.Origin, conn.User.Id)
-	return false
+	return done
 }
